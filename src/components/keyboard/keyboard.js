b3e1ba1e2d0b0520220ea85f13ffd0e20004cde8
Solpio <ssusolpio@gmail.com>
2022-05-11 12:46:58 +0400

    refactor: remove useless module from src

--- src/components/keyboard/keyboard.js
+++
@@ -1,9 +0,0 @@
-import CreateBlock from "../../scripts/createblock";
-
-const CreateKeyboard = function RenderKeyboard(wrapper, textarea){
-    const line = CreateBlock("div", "keyboard__line");
-    const button = CreateBlock("button", "keyboard__button");
-    return [line, button];
-}
-
-export default CreateKeyboard;
